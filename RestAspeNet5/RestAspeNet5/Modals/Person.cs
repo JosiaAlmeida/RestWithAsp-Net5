a14e7aef27d5dc716f92da450f97917e4322b68b
Josia <josiassmith005@gmail.com>
2021-01-27 19:57:24 +0100

    Adicionando Colunas|Modals|Persons

--- RestAspeNet5/RestAspeNet5/Modals/Person.cs
+++ RestAspeNet5/RestAspeNet5/Modals/Person.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RestAspeNet5.Modals
 {
+    [Table("person2")]
     public class Person
     {
+        [Column("id")]
         public long ID { get; set; }
+        [Column("firstname")]
         public string FirstName { get; set; }
+        [Column("lastname")]
         public string LastName { get; set; }
+        [Column("gender")]
         public string Gender { get; set; }
+        [Column("address")]
         public string Adress { get; set; }
 
 
