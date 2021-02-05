eec81f16a5de3895d52f1418a4965a661f6003bd
Josia <josiassmith005@gmail.com>
2021-02-05 18:32:59 +0100

    Classes Genericas

---
+++ RestAspeNet5/RestAspeNet5/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using RestAspeNet5.Modals.Base;
+using RestAspeNet5.Modals.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAspeNet5.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private MySQLContext _context;
+        public GenericRepository(MySQLContext context)
+        {
+            _context = context;
+        }
+        public List<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+        public T FindByID(long ID)
+        {
+            throw new NotImplementedException();
+        }
+        public T Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+        public T Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RestAspeNet5/RestAspeNet5/Repository/Generic/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using RestAspeNet5.Modals;
+using RestAspeNet5.Modals.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAspeNet5.Repository.Generic
+{
+    public interface IRepository<T> where T: BaseEntity 
+    {
+        T Create(T item);
+        List<T> FindAll();
+        T FindByID(long ID);
+        T Update(T item);
+        //T Delete(T item);
+        void Delete(long id);
+    }
+}
